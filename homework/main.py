c1f22a08914166947094b051341bbcb4ef86c7c3
Afanas2707 <avg57327@gmail.com>
2023-11-24 21:38:47 +0300

    Дописал main.py и добавил html документ в репозиторий.

---
+++ homework/main.py
@@ -0,0 +1,11 @@
+import re
+
+with open("Random Email Addresses.html", "r", encoding="utf-8") as file:
+    html = file.read()
+
+# Находим все email адреса на странице
+emails = re.findall(r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}', html)
+
+# Выводим найденные email адреса
+for email in emails:
+    print(emails)
